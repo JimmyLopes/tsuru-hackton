224faf93dd3db8d078e0bb7596d5fb5a434883db
Jimmy Botezine <jbotezine@gmail.com>
2022-09-23 14:06:25 -0300

    feat: add health controller

---
+++ src/main/java/com/digio/hackton/tsuru/backend/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.digio.hackton.tsuru.backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class HealthController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String healtCheck () {
+        return "Sounds Good";
+    }
+}
