03c16d7fcdb88af26855f83d909a3f9a74aaac75
Jimmy Botezine <jbotezine@gmail.com>
2022-09-23 16:38:01 -0300

    refactor: add more logins mocks

--- src/main/java/com/digio/hackton/tsuru/backend/controller/LoginController.java
+++ src/main/java/com/digio/hackton/tsuru/backend/controller/LoginController.java
@@ -9,16 +9,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 @RequestMapping(value = "api/login")
 public class LoginController {
 
     @PostMapping
     public LoginResponse login(@RequestBody LoginRequest loginDTO) {
-        if (loginDTO.getLogin().equals("felipe.oferreira@digio.com.br") && loginDTO.getPassword().equals("Conductor1")) {
+        if (verifyLogin(loginDTO.getLogin(), loginDTO.getPassword())) {
             return new LoginResponse().setMessage("Successful!!");
         } else {
             throw new GenericException(HttpStatus.UNAUTHORIZED, "Error on login, please check you credential and try again");
         }
+    }
+
+    private boolean verifyLogin(String login, String password) {
+        LoginRequest loginReq = new LoginRequest().setLogin("felipe.oferreira@digio.com.br").setPassword("Conductor1");
+        LoginRequest loginTest = new LoginRequest().setLogin("teste@digio.com.br").setPassword("Conductor1");
+
+        return Arrays.asList(loginReq, loginTest).stream().anyMatch(req -> req.getLogin().equals(login) && req.getPassword().equals(password));
     }
 }
